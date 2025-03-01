80e99412f0edade81871c8fd70c54ef529de0660
Karthigagnanavel <147600156+Karthigagnanavel@users.noreply.github.com>
2025-03-01 15:11:12 +0530

    Version 1.0.0

--- CRMS/src/com/app/crms/CRMSApplication.java
+++ CRMS/src/com/app/crms/CRMSApplication.java
@@ -2,7 +2,10 @@ package com.app.crms;
 
 public class CRMSApplication {
 
+	public static String userName ="admin";
+	public static String password ="admin123";
+
 	public static void main(String[] args) {
-		new CriminalPage().startCriminal();
+		new LoginPage().startLogin();
 	}
 }
